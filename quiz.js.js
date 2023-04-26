3dd1059ecd79edab950e25c52e645815c4e51917
33hollie33 <hollybarnhurst@gmail.com>
2023-04-26 19:23:49 +0100

    added css added loop for quiz and buttons

---
+++ quiz.js.js
@@ -0,0 +1,121 @@
+var questions = [
+    {
+        title: 'Commonly used data types DO NOT include',
+        choices: ['strings', 'booleans', 'alerts', 'numbers'],
+        answer: ['alerts']
+    },
+    {
+        title: 'The condition in an if / else statement is enclosed within _____.',
+        choices: ['quotes', 'curly brackets', 'parentheses', 'square brackets'],
+        answer: 'parenteses',
+    },
+    {
+        title: 'Arrays in Javascript can be used to store ____',
+        choices: [
+            'numbers and strings',
+            'other arrays',
+            'booleans',
+            'all of the above',
+        ],
+        answer: 'all of the above',
+    },
+    {
+        title: 'String values must be enclosed within ____ when being assigned to variables',
+        choices: ['commas', 'curly brackets', 'quotes', 'parentheses'],
+        anwser: 'quotes',
+    },
+    {
+        title: 'A very useful tool used during development and debugging for printing content to the debugger is:',
+        choices: ['Javascript', 'terminal/bash', 'for loops', 'console.log'],
+        answer: 'console.log'
+    },
+];
+
+let questionIndex = 0;
+let score = 0;
+let userScores = [];
+
+function displayQuestion() {
+
+    let welcomeScreen = document.getElementById("welcome-screen");
+    if (welcomeScreen != null && welcomeScreen != undefined) {
+        welcomeScreen.style.display = "none";
+    }
+
+    if (questionIndex < questions.length) {
+        let questionFromArray = questions[questionIndex];
+        let message = document.getElementById("message");
+        let question = document.getElementById("quiz-question");
+        question.innerHTML = questionFromArray.title;
+
+        let questionOptions = questionFromArray.choices;
+        let optionsHTML = document.getElementById("options");
+
+        for (let i = 0; i < questionOptions.length; i++) {
+            let optionButton = document.createElement("button");
+            optionButton.innerHTML = questionOptions[i];
+            optionButton.classList.add("option-button");
+            optionsHTML.appendChild(optionButton);
+            optionButton.addEventListener("click", function () {
+                let userChoice = optionButton.innerHTML;
+                if (userChoice == questionFromArray.answer) {
+                    message.innerHTML = "Correct!";
+                    score++;
+                }
+                else {
+                    message.innerHTML = "Wrong!";
+                }
+                questionIndex++;
+                optionsHTML.innerHTML = "";
+                displayQuestion();
+            });
+        }
+    }
+    else {
+        gameOver();
+    }
+}
+function saveScore() {
+    userScores.push(score);
+}
+function playAgain() {
+    score = 0;
+    questionIndex = 0;
+    document.getElementById("quiz-container").style.display = "block";
+    document.getElementById("results-container").style.display = "none";
+    document.getElementById("message").innerHTML = "";
+    displayQuestion();
+    setTimer();
+}
+function setTimer() {
+    let seconds = 60;
+
+    const makeIteration = () => {
+
+        if (seconds > 0) {
+            document.getElementById("timer").innerHTML = "Time remaining: " + seconds;
+            setTimeout(makeIteration, 1000);
+        } else {
+            seconds = 0;
+            document.getElementById("timer").innerHTML = "Time is out";
+            alert("Time is out!");
+            gameOver();
+        }
+        seconds -= 1;
+    }
+
+    setTimeout(makeIteration, 1000);
+}
+
+function startQuiz() {
+    displayQuestion();
+    setTimer();
+}
+
+function gameOver() {
+    document.getElementById("quiz-container").style.display = "none";
+    document.getElementById("results-container").style.display = "block";
+    document.getElementById("summary").innerHTML = "Your final score: " + score;
+}
+
+
---
+++ scores.js
@@ -0,0 +1,5 @@
+for (let i = 0; i <localStorage.length; i++) {
+    let key = localStorage.key(i); 
+    console.log(localStorage.getItem(key));
+   }
+   
